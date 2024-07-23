c06184d66c80bde6cd66ef2a7e13a157e341c574
Yoldaş Ulaş <yutalas@gmail.com>
2024-07-23 13:32:33 +0300

    Fortune Wheel Manager and Fortune Wheel Item View classes are created

---
+++ Assets/Scripts/UI/Fortune Wheel Event/FortuneWheelItemView.cs
@@ -0,0 +1,50 @@
+﻿using Data;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI.Fortune_Wheel_Event
+{
+	public class FortuneWheelItemView : MonoBehaviour
+	{
+		private int rewardAmount;
+		private TextMeshProUGUI rewardAmountText;
+		private float rewardChance;
+		private Image rewardImage;
+
+		private void Awake()
+		{
+			rewardImage = GetComponentInChildren<Image>();
+			rewardAmountText = GetComponentInChildren<TextMeshProUGUI>();
+		}
+
+		private void OnValidate()
+		{
+			if (rewardImage == null)
+			{
+				rewardImage = GetComponentInChildren<Image>();
+			}
+
+			if (rewardAmountText == null)
+			{
+				rewardAmountText = GetComponentInChildren<TextMeshProUGUI>();
+			}
+		}
+
+		public void InitializeReward(SpinRewardData rewardData, float rewardMultiplier)
+		{
+			rewardImage.sprite = rewardData.sprite;
+			rewardChance = rewardData.BaseChance;
+			rewardAmount = (int) (rewardData.BaseAmount * rewardMultiplier);
+
+			if (rewardData.BaseAmount == 0)
+			{
+				rewardAmountText.gameObject.SetActive(false);
+				rewardImage.rectTransform.anchorMin = new Vector2(0, 0);
+				return;
+			}
+			rewardAmountText.text = "X" + rewardAmount;
+
+		}
+	}
+}
---
+++ Assets/Scripts/UI/Fortune Wheel Event/FortuneWheelManager.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Enums;
+using Managers;
+using UnityEngine;
+
+namespace UI.Fortune_Wheel_Event
+{
+	public class FortuneWheelManager : MonoBehaviour
+	{
+		[SerializeField]
+		private GameObject BombExplodedView;
+
+		[SerializeField]
+		private FortuneWheelView fortuneWheelView;
+
+		[SerializeField]
+		private GameObject mainMenuView;
+		private bool isBombScreenOpen;
+
+		private void OnEnable()
+		{
+			EventManager.Instance.AddHandler<int>(GameEvents.OnFortuneWheelReviveButtonClicked, HandleReviveButtonClicked);
+			EventManager.Instance.AddHandler(GameEvents.OnFortuneWheelBombExploded, HandleFortuneWheelBombExploded);
+			EventManager.Instance.AddHandler(GameEvents.OnFortuneWheelExitButtonClicked, HandleExitButtonClicked);
+		}
+
+		private void OnDisable()
+		{
+			if (EventManager.Instance == null)
+				return;
+
+			EventManager.Instance.RemoveHandler<int>(GameEvents.OnFortuneWheelReviveButtonClicked, HandleReviveButtonClicked);
+			EventManager.Instance.RemoveHandler(GameEvents.OnFortuneWheelBombExploded, HandleFortuneWheelBombExploded);
+			EventManager.Instance.RemoveHandler(GameEvents.OnFortuneWheelExitButtonClicked, HandleExitButtonClicked);
+		}
+
+		private void HandleFortuneWheelBombExploded()
+		{
+			isBombScreenOpen = true;
+			BombExplodedView.SetActive(true);
+		}
+
+		private void HandleReviveButtonClicked(int goldAmount)
+		{
+			BombExplodedView.SetActive(false);
+			isBombScreenOpen = false;
+			SaveManager.Instance.ChangeGoldAmount(-goldAmount);
+		}
+
+		private void HandleExitButtonClicked()
+		{
+			if(isBombScreenOpen||fortuneWheelView.IsSpinning)
+				return;
+			EventManager.Instance.Broadcast(GameEvents.OnFortuneWheelExit);
+			BombExplodedView.SetActive(false);
+			gameObject.SetActive(false);
+			mainMenuView.SetActive(true);
+		}
+	}
+}
