1d3c056dacaf74828540730622953ed1e338272e
Yoldaş Ulaş <yutalas@gmail.com>
2024-07-24 16:14:23 +0300

    Fortune Wheel Exit Button and Fortune Wheel Revive Button classes are created

---
+++ Assets/Scripts/UI/Fortune Wheel Event/Button/FortuneWheelExitButton.cs
@@ -0,0 +1,17 @@
+﻿using Enums;
+using Managers;
+using TMPro;
+using UI.Buttons;
+using UnityEngine;
+
+namespace UI.Fortune_Wheel_Event.Button
+{
+	public class FortuneWheelExitButton : UIButton
+	{
+		protected override void DoOnClick()
+		{
+			base.DoOnClick();
+			EventManager.Instance.Broadcast(GameEvents.OnFortuneWheelExitButtonClicked);
+		}
+	}
+}
---
+++ Assets/Scripts/UI/Fortune Wheel Event/Button/FortuneWheelReviveButton.cs
@@ -0,0 +1,42 @@
+﻿using Enums;
+using Managers;
+using TMPro;
+using UI.Buttons;
+using UnityEngine;
+
+namespace UI.Fortune_Wheel_Event.Button
+{
+	public class FortuneWheelReviveButton : UIButton
+	{
+		[SerializeField]
+		private TextMeshProUGUI goldAmountText;
+		[SerializeField]
+		private int initialReviveCost = 100;
+		private int reviveCount = 1;
+
+		private void Awake()
+		{
+			goldAmountText.text = initialReviveCost.ToString();
+		}
+
+		protected override void OnEnable()
+		{
+			base.OnEnable();
+			EventManager.Instance.AddHandler(GameEvents.OnFortuneWheelExit, () => reviveCount=1);
+		}
+
+		protected override void OnDisable()
+		{
+			base.OnDisable();
+			EventManager.Instance.RemoveHandler(GameEvents.OnFortuneWheelExit, () => reviveCount=1);
+		}
+
+		protected override void DoOnClick()
+		{
+			base.DoOnClick();
+			EventManager.Instance.Broadcast(GameEvents.OnFortuneWheelReviveButtonClicked, reviveCount * initialReviveCost);
+			reviveCount++;
+			goldAmountText.text = (reviveCount * initialReviveCost).ToString();
+		}
+	}
+}
