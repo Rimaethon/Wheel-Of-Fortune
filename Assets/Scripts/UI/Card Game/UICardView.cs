f5e45a2a03a2bfd5a814233f3e741e94b6d52786
Yoldaş Ulaş <yutalas@gmail.com>
2024-07-24 11:36:39 +0300

    UI Card View and Currency Panel View classes are created, Currency Panel Prefab is created

---
+++ Assets/Scripts/UI/Card Game/UICardView.cs
@@ -0,0 +1,18 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI.Card_Game_Event
+{
+	public class UICardView : MonoBehaviour
+	{
+		private Image CardImage;
+		private TextMeshProUGUI CardText;
+
+		private void Awake()
+		{
+			CardText = GetComponentInChildren<TextMeshProUGUI>();
+			CardImage = GetComponentInChildren<Image>();
+		}
+	}
+}
---
+++ Assets/Scripts/UI/Currency/CurrencyPanelView.cs
@@ -0,0 +1,67 @@
+﻿using Enums;
+using Managers;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI.Currency
+{
+	public class CurrencyPanelView : MonoBehaviour
+	{
+		[SerializeField]
+		private GameObject goldPanel;
+		[SerializeField]
+		private GameObject cashPanel;
+		private TextMeshProUGUI cashAmountText;
+		private TextMeshProUGUI goldAmountText;
+		private RectTransform rectTransform;
+
+		protected void Awake()
+		{
+			rectTransform = GetComponent<RectTransform>();
+			goldAmountText = goldPanel.GetComponentInChildren<TextMeshProUGUI>();
+			cashAmountText = cashPanel.GetComponentInChildren<TextMeshProUGUI>();
+
+		}
+
+		private void Start()
+		{
+			HandleGoldChange();
+			HandleCashChange();
+		}
+
+		private void OnEnable()
+		{
+			EventManager.Instance.AddHandler(GameEvents.OnGoldAmountChanged, HandleGoldChange);
+			EventManager.Instance.AddHandler(GameEvents.OnCashAmountChanged, HandleCashChange);
+
+		}
+
+		private void OnDisable()
+		{
+			if (EventManager.Instance == null)
+			{
+				return;
+			}
+
+			EventManager.Instance.RemoveHandler(GameEvents.OnGoldAmountChanged, HandleGoldChange);
+			EventManager.Instance.RemoveHandler(GameEvents.OnCashAmountChanged, HandleCashChange);
+		}
+
+		private void HandleGoldChange()
+		{
+			HandleCurrencyString(goldAmountText, SaveManager.Instance.GetGoldAmount());
+		}
+
+		private void HandleCashChange()
+		{
+			HandleCurrencyString(cashAmountText, SaveManager.Instance.GetCashAmount());
+		}
+
+		private void HandleCurrencyString(TextMeshProUGUI amountText, int amount)
+		{
+			amountText.text = amount.ToString("N0");
+			LayoutRebuilder.ForceRebuildLayoutImmediate(rectTransform);
+		}
+	}
+}
