3195db9b0652012a195d815b2e6f34e44ce53d63
Yoldaş Ulaş <yutalas@gmail.com>
2024-07-25 12:05:10 +0300

    Gained Item Panel and Gained Item View classes are created

---
+++ Assets/Scripts/UI/Fortune Wheel Event/Gained Item Panel/FortuneWheelGainedItemPanel.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using Data;
+using UnityEngine;
+
+namespace UI.Fortune_Wheel_Event.Gained_Item_Panel
+{
+	public class FortuneWheelGainedItemPanel : MonoBehaviour
+	{
+		[SerializeField]
+		private GameObject gainedItemPrefab;
+		[SerializeField]
+		private RectTransform content;
+		private readonly List<FortuneWheelGainedItemView> gainedItemViewList = new List<FortuneWheelGainedItemView>();
+		private float itemHeight;
+		private float lastItemYPos = 280;
+
+		private void Awake()
+		{
+			itemHeight = gainedItemPrefab.GetComponent<RectTransform>().rect.height;
+		}
+
+		private void OnDisable()
+		{
+			foreach (FortuneWheelGainedItemView item in gainedItemViewList)
+			{
+				Destroy(item.gameObject);
+			}
+			gainedItemViewList.Clear();
+			lastItemYPos = 280;
+		}
+
+		public Vector3 GetGainedItemIconPosition(SpinRewardData data,float rewardMultiplier)
+		{
+			FortuneWheelGainedItemView fortuneWheelGainedItemView = gainedItemViewList.Find(x => x.itemID == data.ItemData.ItemID && x.itemType == data.ItemData.ItemType);
+
+			if (fortuneWheelGainedItemView == null)
+			{
+				fortuneWheelGainedItemView = Instantiate(gainedItemPrefab, content).GetComponent<FortuneWheelGainedItemView>();
+				gainedItemViewList.Add(fortuneWheelGainedItemView);
+				fortuneWheelGainedItemView.rectTransform.anchoredPosition = new Vector2(0, lastItemYPos);
+				lastItemYPos -= itemHeight + 10;
+				fortuneWheelGainedItemView.itemID = data.ItemData.ItemID;
+				fortuneWheelGainedItemView.itemIcon.sprite = data.sprite;
+				fortuneWheelGainedItemView.itemType = data.ItemData.ItemType;
+
+				//For updating the icon position
+				Canvas.ForceUpdateCanvases();
+			}
+
+			fortuneWheelGainedItemView.amount += (int)(data.BaseAmount*rewardMultiplier);
+			fortuneWheelGainedItemView.amountText.text = fortuneWheelGainedItemView.amount.ToString();
+			return fortuneWheelGainedItemView.itemIcon.rectTransform.position;
+		}
+	}
+}
---
+++ Assets/Scripts/UI/Fortune Wheel Event/Gained Item Panel/FortuneWheelGainedItemView.cs
@@ -0,0 +1,25 @@
+using Enums;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI.Fortune_Wheel_Event.Gained_Item_Panel
+{
+	public class FortuneWheelGainedItemView : MonoBehaviour
+	{
+		public int itemID;
+		public int amount;
+		public TextMeshProUGUI amountText;
+		public Image itemIcon;
+		public ItemTypes itemType;
+		public int Amount;
+		public RectTransform rectTransform;
+
+		private void Awake()
+		{
+			rectTransform = GetComponent<RectTransform>();
+			amountText = GetComponentInChildren<TextMeshProUGUI>();
+			itemIcon = GetComponentInChildren<Image>();
+		}
+	}
+}
